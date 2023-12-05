3e72f12925351a2ae45949b9ae96346ca3fd7a6b
GeorgeOxford <GeorgeLawrence2001@outlook.com>
2023-12-05 17:08:00 +0000

    f

--- question-4-code/random_walk.R
+++ question-4-code/random_walk.R
@@ -1,10 +1,13 @@
-#install.packages("ggplot2")
-#install.packages("gridExtra")
+# install.packages("ggplot2")
+# install.packages("gridExtra")
 
 library(ggplot2)
 library(gridExtra)
 
+
 random_walk  <- function (n_steps) {
+  
+  set.seed(100)
   
   df <- data.frame(x = rep(NA, n_steps), y = rep(NA, n_steps), time = 1:n_steps)
   
@@ -12,13 +15,14 @@ random_walk  <- function (n_steps) {
   
   for (i in 2:n_steps) {
     
-    h <- 0.25
+    stepx <- rnorm(1)
+    stepy <- rnorm(1)
     
     angle <- runif(1, min = 0, max = 2*pi)
     
-    df[i,1] <- df[i-1,1] + cos(angle)*h
+    df[i,1] <- df[i-1,1] + stepx
     
-    df[i,2] <- df[i-1,2] + sin(angle)*h
+    df[i,2] <- df[i-1,2] + stepy
     
     df[i,3] <- i
     
@@ -27,6 +31,7 @@ random_walk  <- function (n_steps) {
   return(df)
   
 }
+
 
 data1 <- random_walk(500)
 
@@ -40,16 +45,5 @@ plot1 <- ggplot(aes(x = x, y = y), data = data1) +
   
   ylab("y-coordinate")
 
-data2 <- random_walk(500)
+plot1
 
-plot2 <- ggplot(aes(x = x, y = y), data = data2) +
-  
-  geom_path(aes(colour = time)) +
-  
-  theme_bw() +
-  
-  xlab("x-coordinate") +
-  
-  ylab("y-coordinate")
-
-grid.arrange(plot1, plot2, ncol=2)
